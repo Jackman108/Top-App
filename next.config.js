00714a760d3e8b7a0edca875830504c775ac43b0
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-01-30 16:42:47 +0300

    add Document

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-	reactStrictMode: true,
-};
+	/* config options here */
+}
 
-export default nextConfig;
+module.exports = nextConfig
