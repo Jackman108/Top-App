6f6b4e9c9e064e882c0391e0d8afa0656b0ffb00
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-02-04 17:44:19 +0300

    Css Layout and Footer

--- layaut/Footer/Footer.tsx
+++ layaut/Footer/Footer.tsx
@@ -1,11 +1,22 @@
 import { FooterProps } from './Footer.props';
-import styles from './Sidebar.module.css';
+import styles from './Footer.module.css';
+import classNames from 'classnames/bind';
+import { format } from 'date-fns';
 
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+
+	const styleNames = classNames.bind(styles);
+	const styleFooter = styleNames(className, styles.footer);
+
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer className={styleFooter}{...props}>
+			<div>
+				OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+			</div>
+			<a href="#" target="_blank">Пользовательское соглашение</a>
+			<a href="#" target="_blank">Политика конфиденциальности </a>
+		</footer>
 	);
 };
--- layaut/Layout.tsx
+++ layaut/Layout.tsx
@@ -5,19 +5,16 @@ import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
 import { FunctionComponent } from 'react';
 
-
 const Layout = ({ children }: LayautProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>
-					{children}
-				</div>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>
+				{children}
 			</div>
-			<Footer />
-		</>
+			<Footer className={styles.footer} />
+		</div>
 	);
 };
 
