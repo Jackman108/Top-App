a66e9833478fb546060107876ff3449f1f82cf9f
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-02-07 20:14:18 +0300

    Adding Sidebar Styling

--- layaut/Sidebar/Sidebar.tsx
+++ layaut/Sidebar/Sidebar.tsx
@@ -1,11 +1,17 @@
 import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.css';
 import { Menu } from '../Menu/Menu';
+import Logo from '../../components/assets/logo.svg';
+import classNames from 'classnames/bind';
 
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+const styleNames = classNames.bind(styles);
+
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
-		<div {...props}>
+		<div className={styleNames(className, styles.sidebar)}  {...props}>
+			<Logo className={styles.logo} />
+			<div>Search</div>
 			<Menu />
 		</div>
 	);
