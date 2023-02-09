ccec71f4a91fd336649b77097099f7fdc6448d27
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-02-09 16:39:41 +0300

    Adding a Sorting Reducer

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -15,7 +15,7 @@ export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Ele
 					[styles.active]: sort == SortEnum.Rating
 				})}
 			>
-				<SortIcon className={styles.sortIcon} /> По&nbsp;рейтингу
+				<SortIcon className={styles.sortIcon} /> По рейтингу
 			</span>
 			<span
 				onClick={() => setSort(SortEnum.Price)}
@@ -23,7 +23,7 @@ export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Ele
 					[styles.active]: sort == SortEnum.Price
 				})}
 			>
-				<SortIcon className={styles.sortIcon} /> По&nbsp;цене
+				<SortIcon className={styles.sortIcon} /> По цене
 			</span>
 		</div>
 	);
