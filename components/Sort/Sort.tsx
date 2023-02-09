a8b3b9060fb160fac477a6533cb28ce18171763a
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-02-09 16:14:32 +0300

    Adding a Sorting Component

---
+++ components/Sort/Sort.tsx
@@ -0,0 +1,30 @@
+import { SortEnum, SortProps } from './Sort.props';
+import styles from './Sort.module.css';
+import SortIcon from './sort.svg';
+import classNames from 'classnames/bind';
+
+
+const styleNames = classNames.bind(styles);
+
+export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
+	return (
+		<div className={styleNames(styles.sort, className)} {...props}>
+			<span
+				onClick={() => setSort(SortEnum.Rating)}
+				className={styleNames({
+					[styles.active]: sort == SortEnum.Rating
+				})}
+			>
+				<SortIcon className={styles.sortIcon} /> По&nbsp;рейтингу
+			</span>
+			<span
+				onClick={() => setSort(SortEnum.Price)}
+				className={styleNames({
+					[styles.active]: sort == SortEnum.Price
+				})}
+			>
+				<SortIcon className={styles.sortIcon} /> По&nbsp;цене
+			</span>
+		</div>
+	);
+};
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,7 +1,8 @@
-import { Advantages, HhData, Htag, Tag } from '@/components';
+import { Advantages, HhData, Htag, Sort, Tag } from '@/components';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { TopLevelCategory } from '@/interfaces/page.interface';
+import { SortEnum } from '@/components/Sort/Sort.props';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
 	return (
@@ -9,7 +10,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 			<div className={styles.title}>
 				<Htag tag='h1'>{page.title}</Htag>
 				{products && <Tag color='grey' size='m'>{products.length}</Tag>}
-				<span>Сортировка</span>
+				<Sort sort={SortEnum.Rating} setSort={() => { }} />
 			</div>
 			<div>
 				{products && products.map(p => (<div key={p._id}>{p.title}</div>))}
