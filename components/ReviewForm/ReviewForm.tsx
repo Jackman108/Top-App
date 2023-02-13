815589420954fc8829169fffd9bec4c2621d7c85
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-02-13 22:21:14 +0300

    Adding a React Hook Form

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -6,22 +6,46 @@ import { Rating } from '../Rating/Rating';
 import { Textarea } from '../Textarea/Textarea';
 import { Button } from '../Button/Button';
 import CloseIcon from './close.svg';
+import { useForm, Controller } from 'react-hook-form';
+import { IReviewForm } from './ReviewForm.interface';
 
 const styleNames = classNames.bind(styles);
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
+	const { register, control, handleSubmit, formState } = useForm<IReviewForm>();
+	const onSubmit = (data: IReviewForm) => {
+		console.log(data);
+	};
 	return (
-		<>
+		<form onSubmit={handleSubmit(onSubmit)}>
 			<div className={styleNames(styles.reviewForm, className)}
 				{...props}
 			>
-				<Input placeholder='Имя' className={styles.name} />
-				<Input placeholder='Заголовок отзыва' className={styles.title} />
+				<Input
+					{...register('name')}
+					placeholder='Имя'
+					className={styles.name}
+				/>
+				<Input
+					{...register('title')}
+					placeholder='Заголовок отзыва'
+					className={styles.title}
+				/>
 				<div className={styles.rating}>
 					<span>Оценка:</span>
-					<Rating rating={0} />
+					<Controller
+						control={control}
+						name='rating'
+						render={({ field }) => (
+							<Rating isEditable rating={field.value} setRating={field.onChange} />
+						)}
+					/>
 				</div>
-				<Textarea placeholder='Текст отзыва' className={styles.description} />
+				<Textarea
+					{...register('description')}
+					placeholder='Текст отзыва'
+					className={styles.description}
+				/>
 				<div className={styles.submit}>
 					<Button appearance='primary'>Отправить</Button>
 					<span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
@@ -34,6 +58,6 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
 				</div>
 				<CloseIcon className={styles.close} />
 			</div>
-		</>
+		</form>
 	);
 };
