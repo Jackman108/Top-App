0ddac11232ff6961d2d7a0e35c3be54212cacd77
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-02-15 13:59:39 +0300

    Fixing the useReducer Bug

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,12 +1,12 @@
 import { CardProps } from './Card.props';
 import styles from './Card.module.css';
 import classNames from 'classnames/bind';
-import { forwardRef } from 'react';
+import { ForwardedRef, forwardRef } from 'react';
 
 
 const styleNames = classNames.bind(styles);
 
-export const Card = forwardRef(({ color = 'white', children, className, ...props }: CardProps, ref: forwardRef<HTMLDivElement>): JSX.Element => {
+export const Card = forwardRef(({ color = 'white', children, className, ...props }: CardProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
 	return (
 		<div className={styleNames(styles.card, className, {
 			[styles.blue]: color == 'blue'
