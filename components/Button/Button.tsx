9482491042fe921250e2fbe1536d914486dab25b
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-02-18 17:36:09 +0300

    Adding Gestures and MotionValues

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,6 +2,8 @@ import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import classNames from 'classnames/bind';
 import ArrowIcon from '../assets/arrow.svg';
+import { motion } from 'framer-motion';
+
 
 
 const styleNames = classNames.bind(styles);
@@ -9,23 +11,21 @@ const styleNames = classNames.bind(styles);
 export const Button = ({ appearance: appearance, arrow = 'none', children, className,
 	...props }: ButtonProps): JSX.Element => {
 
-	const styleButton = styleNames(styles.button, className, {
-		[styles.primary]: appearance == 'primary',
-		[styles.ghost]: appearance == 'ghost'
-	});
-	const styleArrow = styleNames(styles.arrow, {
-		[styles.down]: arrow == 'down'
-	});
-
 	return (
-		<button
-			className={styleButton}
+		<motion.button
+			whileHover={{ scale: 1.05 }}
+			className={styleNames(styles.button, className, {
+				[styles.primary]: appearance == 'primary',
+				[styles.ghost]: appearance == 'ghost'
+			})}
 			{...props}
 		>
 			{children}
-			{arrow !== 'none' && <span className={styleArrow}>
+			{arrow !== 'none' && <span className={styleNames(styles.arrow, {
+				[styles.down]: arrow == 'down'
+			})}>
 				<ArrowIcon />
 			</span>}
-		</button >
+		</motion.button >
 	);
 };  
