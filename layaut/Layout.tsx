a704aea5e133a54902926dc908b8019081de6532
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-02-19 21:33:03 +0300

    Making Menu Accessibility from the Keyboard

--- layaut/Layout.tsx
+++ layaut/Layout.tsx
@@ -3,18 +3,43 @@ import styles from './Layout.module.css';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, KeyboardEvent, useRef, useState } from 'react';
 import { AppContextProvider, IAppContext } from '@/context/app.context';
 import { Up } from '@/components';
+import classNames from 'classnames/bind';
+
+const sn = classNames.bind(styles);
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
+	const [isSkipLinkDisplayed, setIsSkipLinkDisplayed] = useState<boolean>(false);
+	const bodyRef = useRef<HTMLDivElement>(null);
+
+	const skipContentAction = (key: KeyboardEvent) => {
+		if (key.code == 'Space' || key.code == 'Enter') {
+			key.preventDefault();
+			bodyRef.current?.focus();
+		}
+		setIsSkipLinkDisplayed(false);
+	};
 	return (
 		<div className={styles.wrapper}>
+			<a
+				onFocus={() => setIsSkipLinkDisplayed(true)}
+				tabIndex={0}
+				className={sn(styles.skipLink, {
+					[styles.displayed]: isSkipLinkDisplayed
+				})}
+				onKeyDown={skipContentAction}
+			>К содержанию</a>
 			<Header className={styles.header} />
 			<Sidebar className={styles.sidebar} />
-			<div className={styles.body}>
+			<main
+				className={styles.body}
+				ref={bodyRef}
+				tabIndex={0}
+				role='main'>
 				{children}
-			</div>
+			</main>
 			<Footer className={styles.footer} />
 			<Up />
 		</div>
--- layaut/Menu/Menu.tsx
+++ layaut/Menu/Menu.tsx
@@ -1,14 +1,14 @@
 import styles from './Menu.module.css';
 import classNames from 'classnames/bind';
 import { AppContext } from '@/context/app.context';
-import { useContext } from 'react';
+import { KeyboardEvent, useContext } from 'react';
 import { FirstLevelMenuItem, PageItem } from '@/interfaces/menu.interface';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { firstLevelMenu } from '@/helpers/helpers';
 import { motion } from 'framer-motion';
 
-const styleNames = classNames.bind(styles);
+const sn = classNames.bind(styles);
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
@@ -42,14 +42,21 @@ export const Menu = (): JSX.Element => {
 		}));
 	};
 
+	const openSecondLevelKey = (key: KeyboardEvent, secondCategory: string) => {
+		if (key.code == 'Space' || key.code == 'Enter') {
+			key.preventDefault();
+			openSecondLevel(secondCategory);
+		}
+	};
+
 	const buildFirstLevel = () => {
 		return (
-			<>
+			<ul className={styles.firstLevelList}>
 				{firstLevelMenu.map(m => (
-					<div key={m.route}>
+					<li key={m.route}>
 						<Link href={`/${m.route}`}>
 
-							<div className={styleNames(styles.firstLevel, {
+							<div className={sn(styles.firstLevel, {
 								[styles.firstLevelActive]: m.id == firstCategory
 							})}>
 								{m.icon}
@@ -58,65 +65,67 @@ export const Menu = (): JSX.Element => {
 
 						</Link>
 						{m.id == firstCategory && buildSecondLevel(m)}
-					</div>
+					</li>
 				))}
-			</>
+			</ul>
 		);
 	};
 
 	const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
 		return (
-			<div className={styles.secondBlock}>
+			<ul className={styles.secondBlock}>
 				{menu.map(m => {
 					if (m.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
 						m.isOpened = true;
 					}
 					return (
-						<div key={m._id.secondCategory}>
-							<div
+						<li key={m._id.secondCategory}>
+							<button
+								tabIndex={0}
+								onKeyDown={(key: KeyboardEvent) => openSecondLevelKey(key, m._id.secondCategory)}
 								className={styles.secondLevel}
 								onClick={() => openSecondLevel(m._id.secondCategory)}>
 								{m._id.secondCategory}
-							</div>
-							<motion.div
+							</button>
+							<motion.ul
 								layout
 								variants={variants}
 								initial={m.isOpened ? 'visible' : 'hidden'}
 								animate={m.isOpened ? 'visible' : 'hidden'}
-								className={styleNames(styles.secondLevelBlock)}>
+								className={sn(styles.secondLevelBlock)}>
 
-								{buildThirdLevel(m.pages, menuItem.route)}
-							</motion.div>
-						</div>
+								{buildThirdLevel(m.pages, menuItem.route, m.isOpened ?? false)}
+							</motion.ul>
+						</li>
 					);
 				})}
-			</div>
+			</ul>
 		);
 	};
 
-	const buildThirdLevel = (pages: PageItem[], route: string) => {
+	const buildThirdLevel = (pages: PageItem[], route: string, isOpened: boolean) => {
 		return (
 			pages.map(p => (
-				<motion.div
+				<motion.li
 					key={p._id}
 					variants={variantsChildren}
 				>
 					<Link href={`/${route}/${p.alias}`}
-
-						className={styleNames(styles.thirdLevel, {
+						tabIndex={isOpened ? 0 : -1}
+						className={sn(styles.thirdLevel, {
 							[styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath
 						})}>
 						{p.category}
 
 					</Link>
-				</motion.div>
+				</motion.li>
 			))
 		);
 	};
 
 	return (
-		<div className={styles.menu}>
+		<nav className={styles.menu} role='navigation'>
 			{buildFirstLevel()}
-		</div>
+		</nav>
 	);
 };
