40913bf30021c51e37f82b7fc30e139d4f64c38e
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-03-25 14:09:38 +0300

    Added Aria-label & Aria-labelledBy

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -5,23 +5,29 @@ import classNames from 'classnames/bind';
 
 
 const styleNames = classNames.bind(styles);
-
+<div className={styles.sortName} id="sort">Сортировка</div>
 export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
 	return (
 		<div className={styleNames(styles.sort, className)} {...props}>
 			<button
+				id="rating"
 				onClick={() => setSort(SortEnum.Rating)}
 				className={styleNames({
 					[styles.active]: sort == SortEnum.Rating
 				})}
+				area-selected={sort == SortEnum.Rating}
+				area-labelledby="sort rating"
 			>
 				<SortIcon className={styles.sortIcon} /> По рейтингу
 			</button>
 			<button
+				id="price"
 				onClick={() => setSort(SortEnum.Price)}
 				className={styleNames({
 					[styles.active]: sort == SortEnum.Price
 				})}
+				area-selected={sort == SortEnum.Price}
+				area-labelledby="sort price"
 			>
 				<SortIcon className={styles.sortIcon} /> По цене
 			</button>
