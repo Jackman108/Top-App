b5225b91371e33cebc349890cedfc378afe5b803
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-03-26 15:28:40 +0300

    Adding a Meta to the Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+
+function App({ Component, pageProps, router }: AppProps): JSX.Element {
+	return <>;
+
+		<Head>
+			<title>MyTop - наш лучший топ</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+			<meta property='og:locale' content="ru-RU" />
+			<link rel="icon" href="/favicon.ico" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="http://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+		</Head>
+		<Component {...pageProps} />
+	</>;
 }
+
+export default App;
