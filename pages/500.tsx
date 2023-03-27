34f85cb1674a9e2ebfa90201a6e02bfca0e0aa7e
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-03-28 00:20:57 +0300

    Added Pages 404, 500

---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+import { Htag } from "@/components";
+import { withLayout } from "@/layaut/Layout";
+
+function Error500(): JSX.Element {	
+	return (
+		<>
+			<Htag tag='h1'>Ошибка 500</Htag>			
+		</>
+	);
+}
+
+export default withLayout(Error500);
