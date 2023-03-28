862be407c3ce2ad03b15f900bc3fef00dce9cbea
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-03-29 01:44:50 +0300

    Running Via Docker-Compose

--- next.config.js
+++ next.config.js
@@ -3,10 +3,26 @@ module.exports = {
 	images: {
 		domains: ['courses-top.ru']
 	},
-	webpack: (config) => {
+	webpack(config, options) {
 		config.module.rules.push({
+			loader: '@svgr/webpack',
+			issuer: /\.[jt]sx?$/,
+			options: {
+				prettier: false,
+				svgo: true,
+				svgoConfig: {
+					plugins: [{
+						name: 'preset-default',
+						params: {
+							override: {
+								removeViewBox: false
+							}
+						}
+					}],
+				},
+				titleProp: true,
+			},
 			test: /\.svg$/,
-			use: ['@svgr/webpack'],
 		});
 
 		return config;
